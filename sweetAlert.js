14f2065c4f62154b73cec57a78d0069307498e89
Boss599 <yorsse@gmail.com>
2023-02-09 20:05:36 -0400

    add

---
+++ sweetAlert.js
@@ -0,0 +1,20 @@
+Swal.fire({
+    text: "Bienvenido(a) a FOOD DIVCODE",
+  
+    backdrop: "true",
+    timer: 5000,
+    timerProgressBar: true,
+    allowOutsideClick: false,
+    confirmButtonText:"Aceptar",
+    confirmButtonColor:'#f61706'
+  });
+  
+  function aviso() {
+    Swal.fire({
+      icon: "error",
+      text: "No esta habiliadata esta funcion por los momentos",
+      confirmButtonText:"Aceptar",
+      confirmButtonColor:'#f61706'
+    });
+  }
+  
